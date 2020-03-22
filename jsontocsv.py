046717cb58b3eaf92ae32b66b8cfc1c3ca9c0ba2
Jorge Rodríguez Ríos <jorge.rorios@gmail.com>
2020-03-22 18:15:37 +0100

    JSON to CSV converter

---
+++ jsontocsv.py
@@ -0,0 +1,21 @@
+import csv
+import json
+import sys
+
+
+fileInput = 'dataOriginal.json'
+fileOutput = 'data2.csv'
+  
+inputFile = open(fileInput) #open json file
+outputFile = open(fileOutput, 'w') #load csv file
+data = json.load(inputFile) #load json content
+inputFile.close() #close the input file
+
+
+
+
+output = csv.writer(outputFile) #create a csv.write
+output.writerow(data[0].keys())  # header row
+
+for row in data:
+	output.writerow(row.values()) #values row
