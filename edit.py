fb5433422a45f68406da6c137d3e010906e7f0f2
Jorge Rodríguez Ríos <jorge.rorios@gmail.com>
2020-03-22 10:39:32 +0100

    Updated edit file
    I had grouped all keywords and made everything in the same loop

--- edit.py
+++ edit.py
@@ -1,39 +1,26 @@
+
 import csv
 import os
 import sys
 
 #Python that delete the first row in the csv, information about the APP
+#Deleting everything that is unused
 brand = raw_input("Enter your mobile phone brand : ") 
 
 
 lines = list()
 keyWord1 = 'brand=' + brand
+keyWord2 = 'GSM'
+keyWord3 = 'BLE'
 with open('WigleCSV.csv', 'r') as readFile:
 	reader = csv.reader(readFile)
 	for row in reader:
 		lines.append(row)
 		for field in row:
-			if field == keyWord1:
+			if field == keyWord1 or field == keyWord2 or field == keyWord3:
 				lines.remove(row)
 
 with open('mycsv.csv', 'w') as writeFile:
 	writer = csv.writer(writeFile)
 	writer.writerows(lines)
 
-
-#Deleting all the information that make the execution time slow, leaving only WIFI
-
-lines = list()
-keyWord2 = 'GSM'
-keyWord3 = 'BLE'
-with open('mycsv.csv', 'r') as readFile:
-	reader = csv.reader(readFile)
-	for row in reader:
-		lines.append(row)
-		for field in row:
-			if field == keyWord2 or field == keyWord3:
-				lines.remove(row)
-
-with open('mycsv.csv', 'w') as writeFile:
-	writer = csv.writer(writeFile)
-	writer.writerows(lines)
