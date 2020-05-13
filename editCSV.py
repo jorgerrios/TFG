a34a96f46b6edc2f58b7a0f0e00bae7bc22a6e42
Jorge Rodríguez Ríos <jorge.rorios@gmail.com>
2020-05-13 17:10:40 +0200

    Preparing data for the JSON file

---
+++ editCSV.py
@@ -0,0 +1,25 @@
+import csv
+import os
+import sys
+
+#Python that delete the first row in the csv, information about the APP
+#Deleting everything that is unused
+brand = raw_input("Enter your mobile phone brand : ") 
+
+
+lines = list()
+keyWord1 = 'brand=' + brand
+keyWord2 = 'GSM'
+keyWord3 = 'BLE'
+with open('WIGLE.csv', 'r') as readFile:
+	reader = csv.reader(readFile)
+	for row in reader:
+		lines.append(row)
+		for field in row:
+			if field == keyWord1 or field == keyWord2 or field == keyWord3:
+				lines.remove(row)
+
+with open('mycsv.csv', 'w') as writeFile:
+	writer = csv.writer(writeFile)
+	writer.writerows(lines)
+
